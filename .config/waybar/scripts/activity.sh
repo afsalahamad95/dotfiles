1c24e7370bd69ea73c21ee309c0e72fcab1efb16
afsalahamad95 <thenameisafsalahamad@gmail.com>
2026-01-26 11:18:24 +0530

    include btop in waybar

---
+++ .config/waybar/scripts/activity.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cpu=$(awk -v FS=" " '/cpu / {usage=($2+$4)*100/($2+$4+$5)} END {printf "%.0f", usage}' /proc/stat)
+mem=$(free -m | awk '/Mem:/ {printf "%.0f", $3/$2*100}')
+
+echo " 󰍛 ${cpu}% |  ${mem}%"
+
